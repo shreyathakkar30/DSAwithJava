d573dc1ba0b3da491a85640acad48090b5c32b5d
Shreya Thakkar <shreyathakkar2004@gmail.com>
2026-07-06 18:43:15 +0530

    Insertion Sort

--- src/Sorting.java
+++ src/Sorting.java
@@ -11,19 +11,19 @@ public class Sorting {
 
         //time complexity = O(n ^ 2)
         //bubble sort
-//        for (int i=0; i<arr.length-1; i++){
-//            for (int j=0; j<arr.length-i-1; j++){
-//                if (arr[j] > arr[j+1]){
-//                    //swap
-//                    //we can also use bits for swapping
-//                    int temp = arr[j];
-//                    arr[j] = arr[j+1];
-//                    arr[j+1] = temp;
-//                }
-//
-//            }
-//        }
-//        printArray(arr);
+        for (int i=0; i<arr.length-1; i++){
+            for (int j=0; j<arr.length-i-1; j++){
+                if (arr[j] > arr[j+1]){
+                    //swap
+                    //we can also use bits for swapping
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+
+            }
+        }
+        printArray(arr);
 
         //Selection Sort
         //Time complexity = O(n ^ 2)
@@ -37,6 +37,23 @@ public class Sorting {
             int temp = arr[smallest];
             arr[smallest] = arr[i];
             arr[i] = temp;
+        }
+        printArray(arr);
+
+        //Insertion Sort
+        //Time complexity = O(n ^ 2)
+        //Divides array and one part will be sorted and another will be non sorted
+        for (int i=1; i<arr.length; i++){
+            int current = arr[i];
+            int j = i-1;
+            while (j>=0 && current < arr[j]){
+                arr[j+1] = arr[j];
+                j--;
+
+            }
+            //placement
+            arr[j+1] = current;
+
         }
         printArray(arr);
     }
