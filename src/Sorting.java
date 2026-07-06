a4e353bedc10b788905b19f896ec9fb521f402cf
Shreya Thakkar <shreyathakkar2004@gmail.com>
2026-07-06 18:12:13 +0530

    Selection Sort

--- src/Sorting.java
+++ src/Sorting.java
@@ -7,20 +7,36 @@ public class Sorting {
     }
 
     public static void main(String[] args) {
-        int arr[] = {7,8,3,1,2};
+        int arr[] = {7, 8, 3, 1, 2};
 
+        //time complexity = O(n ^ 2)
         //bubble sort
-        for (int i=0; i<arr.length-1; i++){
-            for (int j=0; j<arr.length-i-1; j++){
-                if (arr[j] > arr[j+1]){
-                    //swap
-                    //we can also use bits for swapping
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
+//        for (int i=0; i<arr.length-1; i++){
+//            for (int j=0; j<arr.length-i-1; j++){
+//                if (arr[j] > arr[j+1]){
+//                    //swap
+//                    //we can also use bits for swapping
+//                    int temp = arr[j];
+//                    arr[j] = arr[j+1];
+//                    arr[j+1] = temp;
+//                }
+//
+//            }
+//        }
+//        printArray(arr);
 
+        //Selection Sort
+        //Time complexity = O(n ^ 2)
+        for (int i=0; i<arr.length-1;i++){
+            int smallest = i;
+            for (int j=i+1; j<arr.length; j++){
+                if (arr[smallest] > arr[j]){
+                    smallest = j;
+                }
             }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
         }
         printArray(arr);
     }
