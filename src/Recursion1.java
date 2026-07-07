052f8d3efba9b5bb0b8d61d2de0237c67aff4cf2
Shreya Thakkar <shreyathakkar2004@gmail.com>
2026-07-07 16:56:34 +0530

    Recursion example 2

--- src/Recursion1.java
+++ src/Recursion1.java
@@ -1,10 +1,18 @@
 public class Recursion1 {
+    public static void printNumb1(int m){
+        //question: print 1 to 5
+        if(m == 6){
+            return;//base case
+        }
+        System.out.println(m);
+        printNumb1(m+1);
+    }
     public static void printNumb(int n){
         if (n == 0){
-            return;
+            return;//base case
         }
         System.out.println(n);
-        printNumb(n-1);
+        printNumb(n-1);//recursive
 
     }
 
@@ -12,8 +20,10 @@ public class Recursion1 {
         //What is Recursion: A function calling itself
         //Recursion class 1
         //Question: Print numbers from 5 to 1
-        int n = 5;
+        int n = 5;// fot 5 to 1
+        int m = 1;// for 1 to 5
         printNumb(n); //n = 5
+        printNumb1(m);
 
 
     }
