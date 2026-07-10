9d968a424d1f89bc378e01fe3cb215399c9100ba
Shreya Thakkar <shreyathakkar2004@gmail.com>
2026-07-10 13:20:53 +0530

    Recursion question 3 solved

--- src/Recursion1.java
+++ src/Recursion1.java
@@ -1,4 +1,5 @@
 public class Recursion1 {
+    //question:1
     public static void printNumb1(int m){
         //question: print 1 to 5
         if(m == 6){
@@ -7,12 +8,27 @@ public class Recursion1 {
         System.out.println(m);
         printNumb1(m+1);
     }
+    //question: 2
     public static void printNumb(int n){
         if (n == 0){
             return;//base case
         }
         System.out.println(n);
         printNumb(n-1);//recursive
+
+    }
+    //question:3
+    public static void printSum(int i, int n, int sum){
+        if(i == 9){
+            sum += i;
+            System.out.println(sum);
+            return;
+
+
+        }
+
+        sum += i;
+        printSum(i+1, n, sum);
 
     }
 
@@ -24,6 +40,12 @@ public class Recursion1 {
         int m = 1;// for 1 to 5
         printNumb(n); //n = 5
         printNumb1(m);
+
+        //Question 3: print sum of first n natural numbers
+        //two approaches available which are ascending and descending
+        //base case will be reach to n
+        printSum(1, 5, 0);
+
 
 
     }
